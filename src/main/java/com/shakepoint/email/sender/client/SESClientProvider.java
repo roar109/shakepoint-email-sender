fb74103937fc846b8ce3831de78d43ebce7cee00
Alberto Rubalcaba <arubalcaba@24hourfit.com>
2018-09-21 14:58:16 -0700

    Updated logs for email sender

--- src/main/java/com/shakepoint/email/sender/client/SESClientProvider.java
+++ src/main/java/com/shakepoint/email/sender/client/SESClientProvider.java
@@ -3,41 +3,43 @@ package com.shakepoint.email.sender.client;
 import javax.inject.Inject;
 
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
-import com.amazonaws.services.simpleemail.model.Body;
-import com.amazonaws.services.simpleemail.model.Content;
-import com.amazonaws.services.simpleemail.model.Destination;
-import com.amazonaws.services.simpleemail.model.Message;
-import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+import com.amazonaws.services.simpleemail.model.*;
 import com.github.roar109.syring.annotation.ApplicationProperty;
 import com.github.roar109.syring.annotation.ApplicationProperty.Types;
 import com.github.roar109.syring.annotation.ApplicationProperty.ValueType;
+import org.apache.log4j.Logger;
 
-public class SESClientProvider implements ClientProvider{
+public class SESClientProvider implements ClientProvider {
 
-	@Inject
-	private AmazonSimpleEmailService awsService;
-	
-	@Inject
-	@ApplicationProperty(name="aws.ses.default.from.email", type=Types.SYSTEM, valueType=ValueType.STRING)
-	private String FROM;
+    @Inject
+    private AmazonSimpleEmailService awsService;
 
-	public void send(final String to, final String subject, final String body) {
-		final SendEmailRequest request = new SendEmailRequest()
-		          .withDestination(
-		              new Destination().withToAddresses(to))
-		          .withMessage(new Message()
-		              .withBody(new Body()
-		                  .withHtml(new Content()
-		                      .withCharset("UTF-8")
-		                      .withData(body)
-		                   )
-		               )
-		          .withSubject(new Content()
-		                  .withCharset("UTF-8").withData(subject)
-		                  )
-		          )
-		          .withSource(FROM)
-		          .withReplyToAddresses(FROM);
-		awsService.sendEmail(request);
-	}
+    @Inject
+    @ApplicationProperty(name = "aws.ses.default.from.email", type = Types.SYSTEM, valueType = ValueType.STRING)
+    private String FROM;
+
+    @Inject
+    private Logger log;
+
+    public void send(final String to, final String subject, final String body) {
+        final SendEmailRequest request = new SendEmailRequest()
+                .withDestination(
+                        new Destination().withToAddresses(to))
+                .withMessage(new Message()
+                        .withBody(new Body()
+                                .withHtml(new Content()
+                                        .withCharset("UTF-8")
+                                        .withData(body)
+                                )
+                        )
+                        .withSubject(new Content()
+                                .withCharset("UTF-8").withData(subject)
+                        )
+                )
+                .withSource(FROM)
+                .withReplyToAddresses(FROM);
+        SendEmailResult result = awsService.sendEmail(request);
+        log.info(String.format("Email ID: %s", result.getMessageId()));
+        log.info(String.format("%s", result.getSdkResponseMetadata().toString()));
+    }
 }
